5e5644823e49377ce3464cca38e02fd0c355aa80
Леня Шубин <lenyashubin2002@gmail.com>
2023-11-16 18:16:28 +0200

    Add borderless Entry/Picker mapping to app

--- FinanceApp/App.xaml.cs
+++ FinanceApp/App.xaml.cs
@@ -8,7 +8,27 @@ namespace FinanceApp
         {
             InitializeComponent();
 
-            MainPage = new DashboardView();
+            MainPage = new TransactionsView();
+
+            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(BorderlessEntry), (handler, view) =>
+            {
+#if __ANDROID__
+                    handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent);
+#elif __IOS__
+                    handler.PlatformView.BackgroundColor = UIKit.UIColor.Clear;
+                    handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#endif
+            });
+
+            Microsoft.Maui.Handlers.DatePickerHandler.Mapper.AppendToMapping(nameof(BorderlessDatePicker), (handler, view) =>
+            {
+#if __ANDROID__
+                handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent);
+#elif __IOS__
+                    handler.PlatformView.BackgroundColor = UIKit.UIColor.Clear;
+                    handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#endif
+            });
         }
     }
 }
