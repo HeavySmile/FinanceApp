3bef38726a91ae289e00f203d8b8601c87335aff
Леня Шубин <lenyashubin2002@gmail.com>
2023-11-25 11:22:55 +0200

    Add transaction page functionality

---
+++ FinanceApp/MVVM/Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using FinanceApp.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceApp.MVVM.Models
+{
+    public class Transaction : TableData
+    {
+        public Transaction()
+        {
+            
+        }
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public bool IsIncome { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ FinanceApp/MVVM/ViewModels/TransactionsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using FinanceApp.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceApp.MVVM.ViewModels
+{
+    public class TransactionsViewModel
+    {
+        public Transaction Transaction { get; set; } = new Transaction();
+        
+        public string SaveTransaction()
+        {
+            App.TransactionsRepo.SaveItem(Transaction);
+            return App.TransactionsRepo.StatusMessage;
+        }
+    }
+}
