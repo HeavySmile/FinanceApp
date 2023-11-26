e791124c660a310ed46071346e0dacbd40d9addb
Леня Шубин <lenyashubin2002@gmail.com>
2023-11-26 13:53:24 +0200

    Add dashboard page with its functionality

---
+++ FinanceApp/MVVM/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,43 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using FinanceApp.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceApp.MVVM.ViewModels
+{
+    public partial class DashboardViewModel : ObservableObject
+    {
+        public ObservableCollection<Transaction> Transactions { get; set; }
+        public decimal Balance { get; set; }
+        public decimal Income { get; set; }
+        public decimal Expenses { get; set; }
+
+        public DashboardViewModel()
+        {
+            FillData();
+        }
+
+        private void FillData()
+        {
+            var transactions = App.TransactionsRepo.GetItems();
+            transactions = transactions.OrderByDescending(x => x.Date).ToList();
+            Transactions = new ObservableCollection<Transaction>(transactions);
+
+            Balance = 0;
+            Income = 0;
+            Expenses = 0;
+
+            transactions.ForEach(x => 
+            {
+                if (x.IsIncome) Income += x.Amount;
+                else Expenses += x.Amount;
+            });
+
+            Balance = Income - Expenses;
+        }
+    }
+}
--- FinanceApp/MVVM/Views/DashboardView.xaml.cs
+++ FinanceApp/MVVM/Views/DashboardView.xaml.cs
@@ -1,3 +1,5 @@
+using FinanceApp.MVVM.ViewModels;
+
 namespace FinanceApp.MVVM.Views;
 
 public partial class DashboardView : ContentPage
@@ -5,5 +7,11 @@ public partial class DashboardView : ContentPage
 	public DashboardView()
 	{
 		InitializeComponent();
+		BindingContext = new DashboardViewModel();
 	}
+
+    private async void AddTransaction_Clicked(object sender, EventArgs e)
+    {
+		await Navigation.PushAsync(new TransactionsView());
+    }
 }
