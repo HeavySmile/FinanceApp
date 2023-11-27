ce9dcec7918ddeecbbffea311791d8d371da3706
Леня Шубин <lenyashubin2002@gmail.com>
2023-11-27 17:58:50 +0200

    Add converter classes for currency and color

---
+++ FinanceApp/Converters/AmountToColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceApp.Converters
+{
+    public class AmountToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isIncome = (parameter as Label).Text;
+            var amount = (decimal)value;
+
+            return (isIncome == "True" ? Colors.DarkGreen : Colors.DarkRed);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
