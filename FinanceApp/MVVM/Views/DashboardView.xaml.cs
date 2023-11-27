35efa30b158fe136f2219c36724d54e4b1c14a7f
Леня Шубин <lenyashubin2002@gmail.com>
2023-11-27 14:01:29 +0200

    Bind view model to UI

--- FinanceApp/MVVM/Views/DashboardView.xaml.cs
+++ FinanceApp/MVVM/Views/DashboardView.xaml.cs
@@ -1,17 +1,26 @@
 using FinanceApp.MVVM.ViewModels;
+using PropertyChanged;
 
 namespace FinanceApp.MVVM.Views;
 
 public partial class DashboardView : ContentPage
 {
-	public DashboardView()
-	{
-		InitializeComponent();
-		BindingContext = new DashboardViewModel();
-	}
+    public DashboardView()
+    {
+        InitializeComponent();
+        BindingContext = new DashboardViewModel();
+    }
 
     private async void AddTransaction_Clicked(object sender, EventArgs e)
     {
-		await Navigation.PushAsync(new TransactionsView());
+        await Navigation.PushAsync(new TransactionsView());
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        var vm = (DashboardViewModel)BindingContext;
+        vm.FillData();
     }
 }
+
